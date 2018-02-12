452caa270293f923b0cad1f9ebe33ead06e608d1
Omar Garcia <ogarci79@gmail.com>
2018-02-12 06:22:00 -0700

    Start of pca lesson

---
+++ pca_opencv/src/pca_opencv.cpp
@@ -0,0 +1,14 @@
+#include "pca_opencv.h"
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace std;
+
+int main(int nargs, char *args[])
+{
+    cout << "Hello World" << endl;
+    return 0;
+
+}
+
---
+++ pca_opencv/src/pca_opencv.h
@@ -0,0 +1,9 @@
+#ifndef CPP_pca_opencv_SRC_pca_opencv_H
+#define CPP_pca_opencv_SRC_pca_opencv_H
+
+class pca_opencv
+{
+
+};
+
+#endif // C++_pca_opencv_SRC_pca_opencv_H
